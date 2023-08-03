32fc1bf5dba46f080159f86a82c22eae5619c987
vashu992 <ashutoshpratap992@gmail.com>
2023-08-03 13:58:55 +0530

    tableRouter

---
+++ routes/tableRouter.go
@@ -0,0 +1,14 @@
+package routes
+
+import (
+	controller "github.com/vashu992/Restaurant-Management/controllers"
+
+	"github.com/gin-gonic/gin"
+)
+ 
+func TableRoutes(incomingRoutes *gin.Engine) {
+	incomingRoutes.GET("/tables", controller.GetTables())
+	incomingRoutes.GET("/tables/:order_id", controller.GetTable())
+	incomingRoutes.POST("/tables/", controller.CreateTable())
+	incomingRoutes.PATCH("/tables/:order_id", controller.UpdateTable())
+}
