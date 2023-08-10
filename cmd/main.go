64c8557660d7748e3b6c3b7e311f5c2d35c45b06
vashu992 <ashutoshpratap992@gmail.com>
2023-08-10 14:12:46 +0530

    main.go

---
+++ cmd/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"github.com/vashu992/Restaurant-Management/database"
+	middleware "github.com/vashu992/Restaurant-Management/middleware"
+	routes "github.com/vashu992/Restaurant-Management/routes"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+var foodCollection *mongo.Collection = database.OpenCollection(database.Client, "food")
+
+func main() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "8000"
+	}
+
+	router := gin.New()
+	router.Use(gin.Logger())
+	routes.UserRoutes(router)
+	router.Use(middleware.Authentication())
+
+	routes.FoodRoutes(router)
+	routes.MenuRoutes(router)
+	routes.TableRoutes(router)
+	routes.OrderRoutes(router)
+	routes.OrderItemRoutes(router)
+	routes.InvoiceRoutes(router)
+
+	router.Run(":" + port)
+}
