32ceabd5ef9758e9a925cd395f9833ed3f8df735
vashu992 <ashutoshpratap992@gmail.com>
2023-08-10 13:36:06 +0530

    orderItemRouter

--- routes/orderItemRouter.go
+++ routes/orderItemRouter.go
@@ -8,7 +8,7 @@ import (
  
 func OrderItemRoutes(incomingRoutes *gin.Engine) {
 	incomingRoutes.GET("/orderItems", controller.GetOrderItems())
-	incomingRoutes.GET("/orderItems/:orderItem_id", controller.GetOrderItem())
+	incomingRoutes.GET("/orderItems/:orderItem_id", controller.GetOrderItemsByOrder())
 	incomingRoutes.GET("/orderItems-order/:order_id", controller.GetOrderItemsByOrder())
 	incomingRoutes.POST("/tables/", controller.CreateOrderItem())
 	incomingRoutes.PATCH("/tables/:orderItem_id", controller.UpdateOrderItem())
